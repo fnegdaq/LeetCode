535e8b888242014a9b6b9c18fe75abab74539fbf
fenng <fenngdq@gmail.com>
2020-09-25 17:54:52 +0800

    ConstructBinary

---
+++ 中等 Medium/106.从中序与后序遍历序列构造二叉树ConstructBinaryTree.playground/Contents.swift
@@ -0,0 +1,45 @@
+/**
+ 根据一棵树的中序遍历与后序遍历构造二叉树。
+
+ 注意:
+ 你可以假设树中没有重复的元素。
+
+ 例如，给出
+
+ 中序遍历 inorder = [9,3,15,20,7]
+ 后序遍历 postorder = [9,15,7,20,3]
+ 返回如下的二叉树：
+
+     3
+    / \
+   9  20
+     /  \
+    15   7
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+ }
+
+// 后序最有一位必定为根节点
+// 然后已根节点分割中序为左右子树
+class Solution {
+    func buildTree(_ inorder: [Int], _ postorder: [Int]) -> TreeNode? {
+        var hash = [Int: Int]()
+        var rootNode = postorder.last
+        var result = [Int]()
+        
+        return
+    }
+}

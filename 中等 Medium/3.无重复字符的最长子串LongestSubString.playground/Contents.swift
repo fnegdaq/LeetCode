e4616a2dff27863d0c0fc10d9162d2bb1e507be6
fenng <fenngdq@gmail.com>
2020-10-09 16:40:27 +0800

    LongestSubstringWithoutRepeatingCharacters

---
+++ 中等 Medium/3.无重复字符的最长子串LongestSubString.playground/Contents.swift
@@ -0,0 +1,45 @@
+/**
+ 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+ 示例 1:
+
+ 输入: "abcabcbb"
+ 输出: 3
+ 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ 示例 2:
+
+ 输入: "bbbbb"
+ 输出: 1
+ 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ 示例 3:
+
+ 输入: "pwwkew"
+ 输出: 3
+ 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+func lengthOfLongestSubstring(_ s: String) -> Int {
+    if s.count == 0 { return 0 }
+    var window = [Character: Int]()
+    /// 指针起点, 结束, 最长子串长度
+    var start = 0, end = 0, ans = 0
+    let array = Array(s)
+    while end < s.count {
+        let char = array[end]
+        /// 如果end没有遇到重复继续走, 否则更换start的位置为重复字符next index
+        if let val = window[char] {
+            start = max(start, val)
+        }
+        end += 1
+        ans = max(ans, end - start)
+        window[char] = end
+    }
+    return ans
+}
+
+lengthOfLongestSubstring("pwwkew")
